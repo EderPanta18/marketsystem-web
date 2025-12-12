a649134401e3e4bb90d74071d4d713f44750dd6d
EdeRev <ederpanta181005@gmail.com>
2025-12-12 08:20:14 -0500

    Guardar cambios en atoms index antes de merge

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -1,9 +1,9 @@
 // ui/atoms/index.ts
 
 export { Button } from "./button";
-export { Input } from "./Input";
 export { Icon } from "./icon";
 export { Badge } from "./badge";
 export { Switch } from "./switch";
+export { Input } from "./input";
 export { Spinner } from "./spinner";
 export { Image } from "./image";
